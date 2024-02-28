7c9335c07cea059d1556818421fcfba3d0a6283e
Stephens37 <braydenstephens@protonmail.com>
2024-02-28 06:38:52 -0600

    'Create functions up to indexFunc'

--- linklist.js
+++ linklist.js
@@ -10,13 +10,67 @@ this acts as a factory that will be used by the linkedList factory as the framew
 
 */
 
-function nodeFactory(value, nextNode) {
-  return {
-    value: null,
-    nextNode: null
-  }
+function nodeFactory (value, nextNode) {
+  value = null
+  nextNode = null
+  return { value, nextNode }
 }
 
-function linkedList() {
+const nodeValue = nodeFactory.value
+const nextNodeValue = nodeFactory.nextNode
+
+function linkedList (list, head) {
+
+  head = null
+
+  let append = {
+    appendFunction: function () {
+      list.append(nodeValue, nextNodeValue)
+    }
+  }
+
+  let prepend = {
+    prependFunction: function () {
+      list.insertBefore(nextNodeValue, nodeValue)
+    }
+  }
+
+  let size = {
+    sizeFunction: function () {
+      let sizeNum = 0
+      while (head) {
+        sizeNum++
+        head = head.next
+      }
+      return sizeNum
+    }
+  }
+
+  let headFunc = {
+    headFuncProp: function () {
+      return head
+    }
+  }
+
+  let tailFunc = {
+    tailFuncProp: function () {
+      let tail = head
+      while (tail) {
+        tail = head.next
+      }
+      return tail
+    }
+  }
+  
+  function indexFunc (index) {
+    if(index === 1) {
+      return head
+    }
+    for(let i = 0; i <= index; i++) {
+      index = head.next
+    }
+    return index
+    }
+  }
 
 }
